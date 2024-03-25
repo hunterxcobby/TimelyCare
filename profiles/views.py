abbe4428b04bba8fa951f09cfec92b6b9d328f47
hunterxcobby <solomonsefah13@gmail.com>
2024-03-25 21:44:14 +0000

    Update index view in profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -6,4 +6,5 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def index(request):
-    return Response({"The setup was successful!"})
+    return Response({"This is the api endpoint for the profiles app.": "Welcome to the profiles app."})
+

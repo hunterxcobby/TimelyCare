fa51342a0f46ddc0396658f7c14f03d7e8b7efe8
hunterxcobby <solomonsefah13@gmail.com>
2024-03-25 22:19:27 +0000

    Update database settings in settings.py

--- timelycare/settings.py
+++ timelycare/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'timelycare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'timelycare',
+        'USER': 'cobby',
+        'PASSWORD': 'cobby3136',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

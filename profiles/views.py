78944fdb974266176c38ba58a0d4e4026fda0b9e
hunterxcobby <solomonsefah13@gmail.com>
2024-03-25 21:16:10 +0000

    Add index view to profiles/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import User, Patient, Specialization, Specialist
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    return Response({"The setup was successful!"})

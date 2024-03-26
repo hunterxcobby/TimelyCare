54a4dc3ddf6a33fd7f58fdfa1c34e250cc67bcd3
hunterxcobby <solomonsefah13@gmail.com>
2024-03-26 16:50:09 +0000

    Update ALLOWED_HOSTS in settings.py

--- timelycare/settings.py
+++ timelycare/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'timelycare.onrender.com']
 
 SITE_ID = 1
 

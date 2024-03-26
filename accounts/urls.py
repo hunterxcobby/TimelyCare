0f31086230952b64b50d1333d0750545e8e92b7c
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-03-26 09:33:45 +0000

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('login',views.login, name='login'),
-    path('signup',views.signup, name='signup')
-]
--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def login(request):
-    return render(request, 'login.html')
-
-def signup(request):
-    return render(request, 'signup.html')

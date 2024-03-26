c92809c1767f8b989705e623facaa6c77203d873
Techlord05 <ifeoluwa.adebayo2003@gmail.com>
2024-03-26 15:46:47 +0100

    Created: notification, appointment, medicalhistory, symptom data models and made migrations

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Patient, Specialization, Specialist
+from .models import User, Patient, Specialization, Specialist, Appointment, MedicalHistory, Notification
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Patient)
 admin.site.register(Specialization)
-admin.site.register(Specialist)
+admin.site.register(Appointment)
+admin.site.register(MedicalHistory)
+admin.site.register(Notification)

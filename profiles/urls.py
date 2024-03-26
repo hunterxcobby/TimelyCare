624b38ca5accffa713eb9eba009223d03bcb6443
hunterxcobby <solomonsefah13@gmail.com>
2024-03-26 13:24:57 +0000

    Add new paths for patient, specialization, and specialist views

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
    path('', views.index, name='index'),
    path('users/', views.users, name='users'),
    path('user/add/', views.add_user, name='add_user'),
+   path('patient/<int:user_id>/', views.get_patient, name='get_patient'),
+   path('specialization/<int:specialization_id>/', views.get_specialization, name='get_specialization'),
+   path('specialist/<int:user_id>/', views.get_specialist, name='get_specialist'),
 ]

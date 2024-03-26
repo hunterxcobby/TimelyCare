bd3896012fae8e7cbe23abb5df9887f916502aff
hunterxcobby <solomonsefah13@gmail.com>
2024-03-26 12:54:15 +0000

    Update accounts URL to use allauth

--- timelycare/urls.py
+++ timelycare/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('profiles.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('appointments/', include('appointments.urls')),
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),

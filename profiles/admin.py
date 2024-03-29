945b79d9a3f9b7892f9cc58ec4049681172740ab
hunterxcobby <solomonsefah13@gmail.com>
2024-03-29 12:02:46 +0000

    Remove MedicalHistory from admin site registration

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import User, Patient, Specialization, Specialist, MedicalHistory
+from .models import User, Patient, Specialization, Specialist
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Patient)
 admin.site.register(Specialization)
-admin.site.register(MedicalHistory)
-
+admin.site.register(Specialist)

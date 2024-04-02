e84646587e914599fe91561d8c3e91f7415517d9
Techlord05 <ifeoluwa.adebayo2003@gmail.com>
2024-04-02 13:16:35 +0100

    Add additional paths for notifications and appointments

--- appointments/urls.py
+++ appointments/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('appointments/create/', views.create_appointment, name='create_appointment'),
     path('notifications/', views.notifications, name='notifications'),
     path('notifications/create/', views.create_notification, name='create_notification'),
+    path('notifications/<int:notification_id>/', views.notifications, name='notification'),
 ]
+# Additional paths can be added here
+# For example, to add a path for updating an appointment, you could use:
+#path('appointments/<int:appointment_id>/update/', views.update_appointment, name='update_appointment'),
+
+# Similarly, to add a path for deleting an appointment, you could use:
+#path('appointments/<int:appointment_id>/delete/', views.delete_appointment, name='delete_appointment'),

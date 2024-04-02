dbf141a73cd877b27535b6f51abe353801b99a33
hunterxcobby <solomonsefah13@gmail.com>
2024-04-02 15:51:20 +0000

    Enable update_symptom endpoint in medics/urls.py

--- medics/urls.py
+++ medics/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('symptoms/', views.symptoms, name='symptoms'),
     path('symptoms/<int:symptom_id>/', views.symptoms, name='symptom'),
     path('symptoms/add/', views.add_symptom, name='add_symptom'),
-    # path('symptoms/update/<int:symptom_id>/', views.update_symptom, name='update_symptom'),
+    path('symptoms/update/<int:symptom_id>/', views.update_symptom, name='update_symptom'),
     # path('symptoms/delete/<int:symptom_id>/', views.delete_symptom, name='delete_symptom'),
 ]

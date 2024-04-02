24adc053f563bac373f039c6c7d5889e98c3b0f4
hunterxcobby <solomonsefah13@gmail.com>
2024-04-02 15:52:49 +0000

    Add delete_symptom path to medics/urls.py

--- medics/urls.py
+++ medics/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('symptoms/<int:symptom_id>/', views.symptoms, name='symptom'),
     path('symptoms/add/', views.add_symptom, name='add_symptom'),
     path('symptoms/update/<int:symptom_id>/', views.update_symptom, name='update_symptom'),
-    # path('symptoms/delete/<int:symptom_id>/', views.delete_symptom, name='delete_symptom'),
+    path('symptoms/delete/<int:symptom_id>/', views.delete_symptom, name='delete_symptom'),
 ]

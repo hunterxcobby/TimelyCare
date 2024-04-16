6aa56e5da4ff4631d8eaef29f9429c15e9af3ebb
hunterxcobby <solomonsefah13@gmail.com>
2024-04-16 12:37:56 +0000

    Refactor User and Register serializers in accounts/serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,19 +1,19 @@
-from rest_framework import serializers
-from profiles.models import User, Patient, Specialization, Specialist
+# from rest_framework import serializers
+# from profiles.models import User, Patient, Specialization, Specialist
 
-# User Serializer
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'date_of_birth')
+# # User Serializer
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id', 'email', 'first_name', 'last_name', 'date_of_birth')
 
-# register serializer 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'email','password', 'user_type')
-        extra_kwargs = {'password': {'write_only': True}}
+# # register serializer 
+# class RegisterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id', 'email','password', 'user_type')
+#         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'], validated_data['password'], validated_data['user_type'])
-        return user
+#     def create(self, validated_data):
+#         user = User.objects.create_user(validated_data['email'], validated_data['password'], validated_data['user_type'])
+#         return user

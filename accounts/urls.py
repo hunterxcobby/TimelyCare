35024487f20f4b278a62299de77ca5ead1e1df9a
hunterxcobby <solomonsefah13@gmail.com>
2024-04-16 11:11:23 +0000

    Refactor login endpoint to use obtain_auth_token view from rest_framework.authtoken

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-
-from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('login/',views.login, name='login'),
-    path('signup/',views.signup, name='signup')
+    path('login/', obtain_auth_token, name='login'),
 ]

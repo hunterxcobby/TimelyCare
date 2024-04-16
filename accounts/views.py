f0a7053c0185bb7b609f0d6d3e614cdb01aebe68
hunterxcobby <solomonsefah13@gmail.com>
2024-04-16 11:32:48 +0000

    Add RegisterAPI class to handle user registration in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics,permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer,RegisterSerializer
 
-def login(request):
-    return render(request, 'login.html')
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
 
-def signup(request):
-    return render(request, 'signup.html')
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data,
+            "token":AuthToken.objects.create(user)[1]
+        })

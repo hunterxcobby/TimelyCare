09722807d9add51bea4c4137de86e979a4efeeb0
hunterxcobby <solomonsefah13@gmail.com>
2024-04-16 11:22:02 +0000

    Add Knox authentication and permission classes to REST framework settings

--- timelycare/settings.py
+++ timelycare/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
     'corsheaders',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -176,3 +177,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'knox.auth.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}

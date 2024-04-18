0d2b9b5df7a7efade0d0b0fd21da31ca89a17c22
Techlord05 <ifeoluwa.adebayo2003@gmail.com>
2024-04-18 17:19:05 +0100

    Delete unnecessary files and code

--- timelycare/urls.py
+++ timelycare/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('profiles.urls')),
-    # path('accounts/', include('accounts.urls')),
     path('appointments/', include('appointments.urls')),
     path('admin/', admin.site.urls),
     path('medics/', include('medics.urls')),

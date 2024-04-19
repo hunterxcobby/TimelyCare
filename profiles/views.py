6c0106bc46b02f05c0a7bdad57c88b4d16121acf
Techlord05 <ifeoluwa.adebayo2003@gmail.com>
2024-04-19 14:40:54 +0100

    Refactor login view to remove unnecessary message

--- profiles/views.py
+++ profiles/views.py
@@ -159,12 +159,12 @@ def login(request):
             specialist_info = {
                 "id": user.id
             }
-            return Response({"message": "Redirect to specialist page.", "user_info": specialist_info})
+            return Response({"user_info": specialist_info})
         elif user_type == 'Patient':
             patient_info = {
                 "id": user.id
             }
-            return Response({"message": "Redirect to patient page.", "user_info": patient_info})
+            return Response({"user_info": patient_info})
         else:
             return Response({"message": "Invalid user type."}, status=status.HTTP_400_BAD_REQUEST)
     else:
